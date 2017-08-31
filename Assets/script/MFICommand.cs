68254db8293fcbadcdcd1fa38655e66927ea7b53
caonimeia <302683523@qq.com>
2017-08-31 20:32:36 +0800

    尝试添加命令接口

---
+++ Assets/script/MFICommand.cs
@@ -0,0 +1,33 @@
+﻿
+
+public interface IMFCommand
+{
+    void Execute(object obj);
+}
+
+public class MFMoveCommand : IMFCommand
+{
+    public void Execute(object obj)
+    {
+        var mfMoveable = obj as IMFMoveable;
+
+        if (mfMoveable != null) mfMoveable.Move();
+    }
+}
+
+
+public interface IMFMoveable
+{
+    void Move();
+}
+
+
+
+
+public class Character : IMFMoveable
+{
+    public void Move()
+    {
+
+    }
+}
