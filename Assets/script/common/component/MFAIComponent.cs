ef0d82210697862e7304afa2a754fc934100d046
caonimeia <302683523@qq.com>
2017-09-28 00:42:49 +0800

    粗略地图和随便移动的AI

--- Assets/script/common/component/MFAIComponent.cs
+++ Assets/script/common/component/MFAIComponent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MFAIComponent : MonoBehaviour {
     private MFMoveComponent _moveComp;
+    public float obstacleRange = 5f;
 
     private void Awake() {
         _moveComp = GetComponent<MFMoveComponent>();
@@ -15,6 +16,18 @@ public class MFAIComponent : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        _moveComp.Move(0.1f * Time.deltaTime, 0, 0.1f * Time.deltaTime);
+        transform.Translate(0, 0, 10 * Time.deltaTime);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.75f, out hit)) {
+           
+            if (hit.distance < obstacleRange) {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+
+        
+        //_moveComp.Move(0.1f * Time.deltaTime, 0, 0.1f * Time.deltaTime);
     }
 }
